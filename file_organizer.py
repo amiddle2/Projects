be56deb3cf5a0ca8f07c77cfb4e2d27bc87a8da7
Drew Middleton <164834413+amiddle2@users.noreply.github.com>
2026-01-14 13:57:34 -0600

    Implement file organizer script

---
+++ file_organizer.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import shutil
+
+file_path = 'downloads_folder_path'
+BASE_DIR = Path(file_path) # Use Raw string
+
+
+FILE_TYPES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".webp"],
+    "Documents": [".pdf", ".docx", ".doc", ".txt", ".xlsx", ".pptx"],
+    "Videos": [".mp4", ".mov", ".avi", ".mkv"],
+    "Audio": [".mp3", ".wav", ".aac"],
+    "Archives": [".zip", ".rar", ".tar", ".gz"],
+}
+
+def get_category(file_path: Path) -> str:
+    for category, extensions in FILE_TYPES.items():
+        if file_path.suffix.lower() in extensions:
+            return category
+    return "Others"
+
+def organize_files(base_dir: Path):
+    for item in base_dir.iterdir():
+        if item.is_file():
+            category = get_category(item)
+            target_dir = base_dir / category
+            target_dir.mkdir(exist_ok=True)
+
+            destination = target_dir / item.name
+            shutil.move(str(item), str(destination))
+
+if __name__ == "__main__":
+    organize_files(BASE_DIR)
+    print("Files organized successfully!")
